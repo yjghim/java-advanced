af423689a68ab339fad7ef3e00beeca377b7b240
yjghim <youngjune.ghim@gmail.com>
2022-09-30 15:00:53 +0900

    studied iterator

---
+++ src/collections_framework/iterators/App.java
@@ -0,0 +1,25 @@
+package collections_framework.iterators;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class App {
+    public static void showAll(List<String> list) {
+        ListIterator<String> iterator = list.listIterator();
+
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> languages = new LinkedList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("C++");
+
+        showAll(languages);
+    }
+}
