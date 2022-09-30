8f90dc55f6f911a7370c5a29ca32fa37d5ffc06d
yjghim <youngjune.ghim@gmail.com>
2022-09-30 15:55:12 +0900

    studied compareTo method

---
+++ src/collections_framework/compare_to/App.java
@@ -0,0 +1,10 @@
+package collections_framework.compare_to;
+
+public class App {
+    public static void main(String[] args) {
+        Integer x = 1;
+        Integer y = 2;
+
+        System.out.println(x.compareTo(y));
+    }
+}
