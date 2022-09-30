b8f41dd6e1cba897ce67a78d29557a52509c3fa7
yjghim <youngjune.ghim@gmail.com>
2022-09-30 16:21:49 +0900

    studied comparable interface

---
+++ src/collections_framework/comparable_interface/Name.java
@@ -0,0 +1,30 @@
+package collections_framework.comparable_interface;
+
+public class Name implements Comparable<Name> {
+    private String name = null;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    /*
+    We can customize the logic of compareTo method by overriding.
+    In this case, we hope to compare the two objects by their length.
+     */
+
+    @Override
+    public int compareTo(Name obj) {
+        if(name.length() == obj.name.length())
+            return 0;
+        else if(name.length() == obj.name.length())
+            return -1;
+        else
+            return 1;
+    }
+
+    //  We can customize the logic of toString method by overriding.
+    @Override
+    public String toString() {
+        return name;
+    }
+}
